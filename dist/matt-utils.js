539211488093d59cf4a76e0f8b2ca0e5de2cc27f
mattiacoll <64754556+mattiacoll@users.noreply.github.com>
2021-01-01 18:01:46 +0100

    Updated dependencies

--- dist/matt-utils.js
+++ dist/matt-utils.js
@@ -1,7 +1,7 @@
 (function (global, factory) {
 typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports) :
 typeof define === 'function' && define.amd ? define(['exports'], factory) :
-(global = global || self, factory(global['matt-utils'] = {}));
+(global = typeof globalThis !== 'undefined' ? globalThis : global || self, factory(global['matt-utils'] = {}));
 }(this, (function (exports) {
 function byId(id) {
   return document.getElementById(id);
