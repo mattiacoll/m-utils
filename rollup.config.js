b7637385a71fc0bb925fe7ff6bb7836f88699be7
mattiacoll <64754556+mattiacoll@users.noreply.github.com>
2022-03-22 18:07:57 +0100

    Updated dependencies

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,5 @@
 import babel from '@rollup/plugin-babel';
-import { eslint } from 'rollup-plugin-eslint';
+import eslint from '@rollup/plugin-eslint';
 import { terser } from 'rollup-plugin-terser';
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
